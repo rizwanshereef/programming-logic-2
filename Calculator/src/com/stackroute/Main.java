3ceb6e1d4c825b46f782d5bc4527b06109a0b4fa
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-25 08:17:48 +0000

    updated program calculator

--- Calculator/src/com/stackroute/Main.java
+++ Calculator/src/com/stackroute/Main.java
@@ -9,39 +9,53 @@ public class Main {
         double a,b;
         int num;
         String ch = "y";
-        switch(ch){
-            case "y":{
-                System.out.println(" Enter the first number:");
-                a = s.nextDouble();
-                System.out.println(" Enter the second number:");
-                b= s.nextDouble();
-                System.out.println("Enter number beside the operation to perform: \n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
-                num=s.nextInt();
+        while(true){
+
+            System.out.println(" Enter the first number:");
+            a = s.nextDouble();
+            System.out.println(" Enter the second number:");
+            b= s.nextDouble();
+            System.out.println("Enter number beside the operation to perform: \n1. Add\n2. Subtract\n3. Multiply\n4. Divide");
+            num=s.nextInt();
                 switch(num) {
                     case 1:
                         System.out.println(+a + "+" + b + "=" + (a + b));
-                        break;
+                        System.out.println("Do you want to try again(y/n):");
+                        ch = s.next();
+                        if(ch.equals("y")) {
+                            break;
+                        }
+                        else System.exit(0);
                     case 2:
                         System.out.println(+a + "-" + b + "=" + (a - b));
-                        break;
+                        System.out.println("Do you want to try again(y/n):");
+                        ch = s.next();
+                        if(ch.equals("y")) {
+                            break;
+                        }
+                        else System.exit(0);
                     case 3:
                         System.out.println(+a + "*" + b + "=" + (a * b));
-                        break;
+                        System.out.println("Do you want to try again(y/n):");
+                        ch = s.next();
+                        if(ch.equals("y")) {
+                            break;
+                        }
+                        else System.exit(0);
                     case 4:
                         System.out.println(+a + "/" + b + "=" + (a / b));
-                        break;
+                        System.out.println("Do you want to try again(y/n):");
+                        ch = s.next();
+                        if(ch.equals("y")) {
+                            break;
+                        }
+                        else System.exit(0);
                     default: {
                         System.out.println("Enter a valid number!!");
                     }
                 }
-                System.out.println("Do you want to try again(y/n):");
-                ch = s.next();
+
             }
-            case "n":
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + ch);
         }
 
     }
-}
