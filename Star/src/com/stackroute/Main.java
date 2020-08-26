79eab8cd278657e553c5b90b78d14f3ba887f5bb
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 23:42:14 +0530

    Update comments

--- Star/src/com/stackroute/Main.java
+++ Star/src/com/stackroute/Main.java
@@ -2,25 +2,24 @@ package com.stackroute;
 
 public class Main {
 
-    public static void main(String args[])
-    {
+/* Main class to print sequence of * */
+    public static void main(String args[]){
         Main m = new Main();
         int n = 5;
+/*Invoke method stars*/
         Stars(n);
     }
 
-    public static void Stars(int n)
-    {
+/*Method to print * sequence */
+    public static void Stars(int n){
         int i, j;
-        for(i=0; i<n; i++)
-        {
-            for(j=2*(n-i); j>=0; j--)
-            {
+/*Print * using 3 loops based on number of each value of n*/
+        for(i=0; i<n; i++){
+            for(j=2*(n-i); j>=0; j--){
                 System.out.print(" ");
             }
 
-            for(j=0; j<=i; j++)
-            {
+            for(j=0; j<=i; j++){
                 System.out.print("* ");
             }
 
