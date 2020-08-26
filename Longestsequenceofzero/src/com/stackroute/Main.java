854168b89f1a8b68c0ffd1f382cb831fe9323cb4
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 23:26:18 +0530

    Update comments

--- Longestsequenceofzero/src/com/stackroute/Main.java
+++ Longestsequenceofzero/src/com/stackroute/Main.java
@@ -1,16 +1,17 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/*Main class to check longest sequence of zero by converting decimail to variable*/
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+/*Insert number*/
         System.out.println("Enter the number:");
         int N = s.nextInt();
+/*Initialize count variable*/        
         int maxm = -1;
-
         int count = 0;
-
+/*Convert and check sequence while increasing count variable*/
         while (N != 0) {
             if ((N & 1) == 0 ) {
                 count++;
