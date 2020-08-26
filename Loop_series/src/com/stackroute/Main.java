fb8b73b5aa3a66342b42e33fc760902759467509
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 23:28:49 +0530

    Update comments

--- Loop_series/src/com/stackroute/Main.java
+++ Loop_series/src/com/stackroute/Main.java
@@ -1,14 +1,15 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/*Class to print sequence of * */
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+/*Enter the length of the tree level*/
         System.out.println("Enter the value of n: ");
         int number = sc.nextInt();
+/* * generation using 2 loops */
         for (int i = 1; i <= number; i++) {
             for (int j = 1; j <= i; j++)
                 System.out.print(i + " ");
