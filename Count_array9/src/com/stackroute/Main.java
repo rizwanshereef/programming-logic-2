196929b1a79da28fa28e2098ead5e6b520cbbbac
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 23:22:58 +0530

    Update comment

--- Count_array9/src/com/stackroute/Main.java
+++ Count_array9/src/com/stackroute/Main.java
@@ -4,28 +4,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+/*main class to check the count of 9s in the array*/
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the no of elements");
         int n = s.nextInt();
+    /*Array insert*/
         int num[] = new int[n];
         for (int i = 0; i < n; i++) {
             System.out.println("Enter the " + (i + 1) + "Number:");
             num[i] = s.nextInt();
         }
+/*passing the array to method arrayCount*/
         arrayCount(num);
 
 
     }
 
-    public static void arrayCount(int[] num)
-    {
+/*Method to count the no of 9 on the array*/
+    public static void arrayCount(int[] num){
+/*Initialize count variable*/
         int count = 0;
         int n = num.length;
-
-        for(int i =0; i<n;i++)
-        {
+/*Checking each value of array for 9 then increament counter*/
+        for(int i =0; i<n;i++){
             if(num[i]==9)
             {
                 count++;
