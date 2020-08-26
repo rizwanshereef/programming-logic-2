1f5c3dc486806733b3f457a87b0c5b403bf36f74
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 23:52:12 +0530

    Update comments

--- sumofcommonarray/src/com/stackroute/Main.java
+++ sumofcommonarray/src/com/stackroute/Main.java
@@ -3,7 +3,7 @@ package com.stackroute;
 import java.util.Scanner;
 
 public class Main {
-
+/*Main class to find the sum of common elements*/
     public static void main(String[] args) {
         Main m = new Main();
         Scanner s= new Scanner(System.in);
@@ -12,39 +12,35 @@ public class Main {
         n = s.nextInt();
         int arr1[] = new int[n];
         int arr2[] = new int[n];
-        for(i=0;i<n;i++)
-        {
+/*Input values into 2 arrays*/
+        for(i=0;i<n;i++){
             System.out.println("Enter the"+" "+(i+1)+" number of the 1st Array");
             arr1[i]=s.nextInt();
         }
-        for(i=0;i<n;i++)
-        {
+        for(i=0;i<n;i++){
             System.out.println("Enter the" + " " + (i + 1) + " number of the 2nd Array");
             arr2[i] = s.nextInt();
         }
+/*Call method to find common sum*/
         m.sum(arr1, arr2);
     }
 
-    public void sum(int[] arr1, int[] arr2)
-    {
+/*Method to find sum of common number in array*/
+    public void sum(int[] arr1, int[] arr2){
         int sum=0;
-
-        for(int i=0; i<arr1.length; i++)
-        {
-            for(int j=0;j< arr2.length;j++)
-            {
+/*Check both arrays for common element and increament sum counter*/
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0;j< arr2.length;j++){
                 if(arr1[i]==arr2[j])
                 {
                     sum = sum + arr1[i];
                 }
             }
         }
-        if(sum==0)
-        {
+        if(sum==0){
             System.out.println("No Common Elements");
         }
-        else
-        {
+        else{
             System.out.println("Sum of common elements of the 2 Arrays: "+sum);
         }
 
