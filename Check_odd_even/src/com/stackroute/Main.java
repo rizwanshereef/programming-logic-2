827f79ae755c6e0b6a0b2483d386f1cca726d777
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-26 23:18:43 +0530

    Update comments

--- Check_odd_even/src/com/stackroute/Main.java
+++ Check_odd_even/src/com/stackroute/Main.java
@@ -1,14 +1,16 @@
 package com.stackroute;
 
 import java.util.Scanner;
-
+/*Class to check whether the entered no is odd or even and is between 20 and 30*/
 public class Main {
 
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
+/*Input number*/
         System.out.println("Enter the number");
         int num = s.nextInt();
+/*Check if the number is odd or even and satisfies the condition*/
         if(num % 2== 0 && num > 20 && num <30)
         {
             System.out.println("Jerry");
